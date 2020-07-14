181ca026c0d0e1c93e7bb5b50f97f0fc2a70616f
Nikita <jrringler13759@gmail.com>
2020-07-14 11:11:58 -0400

    added indexedDB connection

--- public/db.js
+++ public/db.js
@@ -18,7 +18,7 @@ request.onsuccess = function(event) {
 };
 
 request.onerror = function(event) {
-  console.log("Woops! " + event.target.errorCode);
+  console.log("An error has occured: " + event.target.errorCode);
 };
 
 function saveRecord(record) {
